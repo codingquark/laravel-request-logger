fb084d1f19a298de4a616686b836dbad35d69101
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2016-05-25 11:16:54 +0530

    Logging request content

--- src/Prettus/RequestLogger/Helpers/RequestInterpolation.php
+++ src/Prettus/RequestLogger/Helpers/RequestInterpolation.php
@@ -85,6 +85,12 @@ class RequestInterpolation extends BaseInterpolation {
                 case "date":
                     $matches[] = "clf";
                     break;
+                case "request-content":
+                    if($data = $this->request->all())
+                        return json_encode($data);
+                    else
+                        return "";
+                    break;
                 }
             }
 
